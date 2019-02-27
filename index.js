0420fe613b966998c043a0a65f9111f4b8e3997c
cemremengu <cemremengu@gmail.com>
2019-02-27 12:20:08 +0300

    Set sourceType

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = {
     'node': true,
     'es6': true
   },
+  parserOptions: {
+    'sourceType': 'script'
+  },
   rules: {
     'strict': ['error', 'global'],
     'func-names': 'off',
