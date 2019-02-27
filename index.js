b13e5df215d1e653ae218182ec050949057bb447
Cemre Mengu <cemremengu@gmail.com>
2019-02-27 11:55:10 +0300

    Require strict directive

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   rules: {
+    'strict': ['error', 'global'],
     'func-names': 'off',
     'no-restricted-syntax': 'off',
     'global-require': 'off',
