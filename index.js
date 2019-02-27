895c6fb8f94fa76dda5f68d33f2c46705d81c3ea
cemremengu <cemremengu@gmail.com>
2019-02-27 12:11:42 +0300

    Set env as node

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  env: {
+    'node': true,
+    'es6': true
+  },
   rules: {
     'strict': ['error', 'global'],
     'func-names': 'off',
